ba51280d81d666cf7e0b2d1ee13818f5855cf53c
Leeseunghwan <sun1238799@naver.com>
2021-09-12 00:33:34 +0900

    study

--- server.js
+++ server.js
@@ -29,15 +29,27 @@ app.get("/", function (요청, 응답) {
 
 app.post("/add", function (요청, 응답) {
   응답.send("전송완료");
-  console.log(요청.body.date);
-  console.log(요청.body.title);
-  db.collection("post").insertOne(
-    {
-      제목: 요청.body.title,
-      날짜: 요청.body.date,
-    },
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
     function (에러, 결과) {
-      console.log("저장완료");
+      console.log(결과.totalPost);
+      let 총게시물갯수 = 결과.totalPost;
+      db.collection("post").insertOne(
+        {
+          _id: 총게시물갯수 + 1,
+          제목: 요청.body.title,
+          날짜: 요청.body.date,
+        },
+        function (에러, 결과) {
+          console.log("저장완료");
+
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            { $inc: { totalPost: 1 } },
+            function () {}
+          );
+        }
+      );
     }
   );
 });
